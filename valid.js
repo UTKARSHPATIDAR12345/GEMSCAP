f4cb465704b146d4188ecbeb9d56cd0103667070
aadarshjain <62375555+aadarshjain@users.noreply.github.com>
2020-08-15 18:39:06 +0530

    latest

--- valid.js
+++ valid.js
@@ -4,17 +4,17 @@ var password = document.forms['form']['password'];
 var email_error = document.getElementById('email_error')
 var pass_error = document.getElementById('pass_error')
 
-email.addEventListener('textInput', email_Verify);
-password.addEventListener('textInput', pass_Verify);
+email.addEventListener('textInput', email_verify);
+password.addEventListener('textInput', pass_verify);
 
 function validated(){
-	if (email.value.length < 9) {
+	if (email.value != 'admin') {
 		email.style.border = "1px solid red";
 		email_error.style.display = "block";
 		email.focus();
 		return false;
 	}
-	if (password.value.length < 9) {
+	if (password.value != 'admin') {
 		password.style.border = "1px solid red";
 		pass_error.style.display = "block";
 		password.focus();
@@ -22,15 +22,16 @@ function validated(){
 	}	
 }
 function email_Verify() {
-	if (email.value.length >= 8) {
+	if (email.value.length == 'admin') {
 		email.style.border = "1px solid silver";
 		email_error.style.display = "none";
 		return true;	
 	}
 }
 function pass_Verify() {
-	if (password.value.length >= 5) {
+	if (password.value == 'admin') {
 		password.style.border = "1px solid silver";
 		pass_error.style.display = "none";
 		return true;	
-	}
+    }
+}
